56ed3dbec321f317ff411ab6278d5edf4bc32d26
jraitter <jraitter@cableone.net>
2020-04-24 09:22:56 -0600

    Added Rating field to database

--- Migrations/20200423022313_InitialCreate.cs
+++ Migrations/20200423022313_InitialCreate.cs
@@ -3,31 +3,32 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace MvcMovie.Migrations
 {
-    public partial class InitialCreate : Migration
+  public partial class InitialCreate : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.CreateTable(
-                name: "Movie",
-                columns: table => new
-                {
-                    Id = table.Column<int>(nullable: false)
-                        .Annotation("Sqlite:Autoincrement", true),
-                    Title = table.Column<string>(nullable: true),
-                    ReleaseDate = table.Column<DateTime>(nullable: false),
-                    Genre = table.Column<string>(nullable: true),
-                    Price = table.Column<decimal>(nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_Movie", x => x.Id);
-                });
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropTable(
-                name: "Movie");
-        }
+      migrationBuilder.CreateTable(
+          name: "Movie",
+          columns: table => new
+          {
+            Id = table.Column<int>(nullable: false)
+                  .Annotation("Sqlite:Autoincrement", true),
+            Title = table.Column<string>(nullable: true),
+            ReleaseDate = table.Column<DateTime>(nullable: false),
+            Genre = table.Column<string>(nullable: true),
+            Rating = table.Column<string>(nullable: true),
+            Price = table.Column<decimal>(nullable: false)
+          },
+          constraints: table =>
+          {
+            table.PrimaryKey("PK_Movie", x => x.Id);
+          });
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+          name: "Movie");
+    }
+  }
 }
--- Migrations/MvcMovieContextModelSnapshot.cs
+++ Migrations/MvcMovieContextModelSnapshot.cs
@@ -7,38 +7,41 @@ using MvcMovie.Data;
 
 namespace MvcMovie.Migrations
 {
-    [DbContext(typeof(MvcMovieContext))]
-    partial class MvcMovieContextModelSnapshot : ModelSnapshot
+  [DbContext(typeof(MvcMovieContext))]
+  partial class MvcMovieContextModelSnapshot : ModelSnapshot
+  {
+    protected override void BuildModel(ModelBuilder modelBuilder)
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
-        {
 #pragma warning disable 612, 618
-            modelBuilder
-                .HasAnnotation("ProductVersion", "3.1.3");
+      modelBuilder
+          .HasAnnotation("ProductVersion", "3.1.3");
 
-            modelBuilder.Entity("MvcMovie.Models.Movie", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("INTEGER");
+      modelBuilder.Entity("MvcMovie.Models.Movie", b =>
+          {
+            b.Property<int>("Id")
+                      .ValueGeneratedOnAdd()
+                      .HasColumnType("INTEGER");
 
-                    b.Property<string>("Genre")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Genre")
+                      .HasColumnType("TEXT");
 
-                    b.Property<decimal>("Price")
-                        .HasColumnType("TEXT");
+            b.Property<string>("Rating")
+                .HasColumnType("TEXT");
 
-                    b.Property<DateTime>("ReleaseDate")
-                        .HasColumnType("TEXT");
+            b.Property<decimal>("Price")
+                      .HasColumnType("TEXT");
 
-                    b.Property<string>("Title")
-                        .HasColumnType("TEXT");
+            b.Property<DateTime>("ReleaseDate")
+                      .HasColumnType("TEXT");
 
-                    b.HasKey("Id");
+            b.Property<string>("Title")
+                      .HasColumnType("TEXT");
 
-                    b.ToTable("Movie");
-                });
+            b.HasKey("Id");
+
+            b.ToTable("Movie");
+          });
 #pragma warning restore 612, 618
-        }
     }
+  }
 }
